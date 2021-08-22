f6eb23dc93be4ef87ce006a5ba4c483d8b920f37
justg3t2it <86816710+justg3t2it@users.noreply.github.com>
2021-08-22 15:51:08 -0700

    updated files

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,15 @@
+def fibby(n): 
+    n = int(n)
+    if n < 0: 
+        print("Negative numbers? Who do you think I am? Try again next month.")
+    
+    if n <= 1: 
+        return n
+    else: 
+        return(fibby(n-1) + fibby(n-2))
+        
+    n_range = n
+    
+    for i in range(n_range):
+            return fibby(i)
 
--- fibonacci_test.py
+++ fibonacci_test.py
@@ -1,1 +1,19 @@
+import pytest
+from fibonacci import fibby
+
+def testFirst():
+    assert fibby(1) == 1
+
+def testZero():
+    assert fibby(0) == 0
+
+def testOther():
+    assert fibby(5) == 5
+
+def testEight():
+    assert fibby(8) == 21
+
+def testNegative():
+    assert fibby(-5) == 1
+
 
