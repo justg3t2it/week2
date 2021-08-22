46dbcb315a5a390c571e61993d2049bd6236edeb
justg3t2it <86816710+justg3t2it@users.noreply.github.com>
2021-08-22 14:57:34 -0700

    pushing updated files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # week2
+
+# Palindrome.py attempts to find the longest palindrome within a string
+# I used re to make the string lower case and remove punctuation to make sure it addressed 
+# the following use cases:
+# palindromes with whitespace
+# palindromes with punctuation in between
+
+# palindrome_test.py 
+# tested for different cases using pytest
+# testing for empty string
+# testing for single character string
+# testing for even char in string
+# testing for odd char in string
+# testing for white space
+# testing for punctuation
--- palindrome.py
+++ palindrome.py
@@ -17,7 +17,7 @@ def longestPal(s):
     s = s.lower()
     st = re.sub(r'[^\w]', '', s)
     st = st.replace(" ", "")
-    table = [[ 0 for i in range(len(st))] for i in range(len(st))]
+    table = [[ 0 for i in range(len(st))] for i in range(len(st))] #table length x length of string
 
 
     for p_length in range(0, len(st)):
@@ -27,11 +27,11 @@ def longestPal(s):
     
     
         while right < len(st):
-            if left == right: 
+            if left == right: #only one char
                 table[left][right] = 1
             else: 
                 if st[left] == st[right]: #even use case
-                    if left == right -1: #account for 0 start value
+                    if left == right -1: #account for start value to stay within range
                         table[left][right] = table[left+1][right-1] + 1
                     else: 
                         table[left][right] = table[left+1][right-1] + 2
@@ -40,7 +40,7 @@ def longestPal(s):
 
             if table[left][right] > pal_length:
                 pal_length = table[left][right]
-                longest_Palindrome = st[left:right+1]
+                longest_Palindrome = st[left:right+1] #account to stay within range
             
 
             left += 1
